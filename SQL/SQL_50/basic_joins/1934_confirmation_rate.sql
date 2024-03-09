22672a0d2e914ce79b517a9607230f8d9a583d54
naity <ytiancompbio@gmail.com>
2024-03-09 09:39:01 -0800

    03092024

--- SQL/SQL_50/basic_joins/1934_confirmation_rate.sql
+++ SQL/SQL_50/basic_joins/1934_confirmation_rate.sql
@@ -1,13 +1,10 @@
-Select s.user_id,
+SELECT s.user_id,
     ROUND(
-        SUM(
-            CASE
-                WHEN action = 'confirmed' THEN 1
-                ELSE 0
-            END
-        ) / COUNT(*),
+        AVG(
+            IF(c.action = 'confirmed', 1, 0)
+        ),
         2
     ) AS confirmation_rate
 FROM Signups s
     LEFT JOIN Confirmations c ON s.user_id = c.user_id
-GROUP BY s.user_id
+GROUP BY s.user_id;
